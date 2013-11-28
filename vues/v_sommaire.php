90bc3e23450e4887f34fc171f72f378d41e858de
cdeboeuf <deboeuf.cecile@gmail.com>
2013-11-28 15:48:45 +0100

    Connection presque terminé

--- vues/v_sommaire.php
+++ vues/v_sommaire.php
@@ -5,9 +5,11 @@
         <h2>
     
 </h2>
-    
-      </div>  
+        </div>  
         <ul id="menuList">
+            <?php if($_SESSION['type']=='v')
+            {          
+?>
 			<li >
 				  Visiteur :<br>
 				<?php echo $_SESSION['prenom']."  ".$_SESSION['nom']  ?>
@@ -21,6 +23,17 @@
  	   <li class="smenu">
               <a href="index.php?uc=connexion&action=deconnexion" title="Se déconnecter">Déconnexion</a>
            </li>
+            <?php } 
+            else if ($_SESSION['type']=='c'){?>
+           
+			<li >
+				  Comptable :<br>
+				<?php echo $_SESSION['prenom']."  ".$_SESSION['nom']  ?>
+			</li>
+                         <li class="smenu">
+              <a href="index.php?uc=validerFrais&action=..." title="Se déconnecter">Valider fiche de frais</a>
+           </li>
+           <?php } ?>
          </ul>
         
     </nav>
